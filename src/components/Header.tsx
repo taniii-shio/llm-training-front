c372e3d4e8b2632557eec6667950c7b6112230c8
Naoto Shioya <shioya.naoto17@gmail.com>
2023-11-10 12:59:53 +0900

    delete icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { BsMoon, BsSun } from 'react-icons/bs';
+// import { BsMoon, BsSun } from "react-icons/bs";
 
 type HeaderProps = {
   // children: React.ReactNode;
 };
 
 export const Header: React.FC<HeaderProps> = ({}) => {
- const isDarkMode = false;
+  // const isDarkMode = false;
 
   return (
-  <header className="px-6 py-6 border-b bg-white">
-    <div className="flex justify-between">
-      <section><span className="text-xl text-slate-800 font-bold">ArXiv Search</span></section>
-      <section>
-        <ul>
-          <li>
-            <button>{isDarkMode ? <BsSun /> : <BsMoon />}</button>
-          </li>
-        </ul>
-      </section>
-    </div>
-  </header>
+    <header className="px-6 py-6 border-b bg-white">
+      <div className="flex justify-between">
+        <section>
+          <span className="text-xl text-slate-800 font-bold">ArXiv Search</span>
+        </section>
+        <section>
+          <ul>
+            <li>
+              {/* <button>{isDarkMode ? <BsSun /> : <BsMoon />}</button> */}
+            </li>
+          </ul>
+        </section>
+      </div>
+    </header>
   );
 };
 
